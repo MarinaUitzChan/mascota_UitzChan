e3fa6d6bc9806dd5ad758b1480e90981f04d9016
MarinaUitzChan <marinauitzchan@gmail.com>
2021-10-14 23:44:41 -0500

    Agregación de BelongsTo
    Se le agrego el belongsTo  para relacionar los datos

--- app/Mascota.php
+++ app/Mascota.php
@@ -6,13 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mascota extends Model
 {
-    //
-      protected $tabel='mascotas';
+    //Nombre de la tabla
+    protected $tabel='mascotas';
     //Se especifica la clave primaria
     protected $primaryKey='id_mascota';
+    //Relaciones
+    protected $with=['especie', 'raza'];
+    //Se coloca true cuando la clave primaria sea númerica
+    public $incrementing=true;
+    //Se coloca cuando quieras poner etiqueta de tiempo.
+    public $timestamps=false;
 
-    protected $with=['especie', 'raza' , 'propietario'];
-
+    //campos que se van a tomar valor
     public $fillable=[
     	'id_mascota',
     	'nombre',
@@ -25,4 +30,16 @@ class Mascota extends Model
     	'id_raza'
 
     ];
+    //Descripción de relacion
+    public function especie()
+    {
+        //Se utiliza cuando se desea relacionar un registro hijo con solo un elemento padre.
+        return $this-> belongsTo(Especie::class, 'id_especie', 'id_especie');
+    }
+
+    public function raza()
+    {
+        return $this->belongsTo(Raza::class, 'id_raza', 'id_raza');
+    };
+
 }
