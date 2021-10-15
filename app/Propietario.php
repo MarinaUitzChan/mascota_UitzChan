1153493561e4ad2db35f08b44560f5b02c603ab3
MarinaUitzChan <marinauitzchan@gmail.com>
2021-10-14 23:42:36 -0500

    Modificación y agregamiento de datos.
    Se modifico el y se le agregaron algunos datos

--- app/Propietario.php
+++ app/Propietario.php
@@ -10,6 +10,10 @@ class Propietario extends Model
     protected $tabel='propietario';
     //Se especifica la clave primaria
     protected $primaryKey='id_propietario';
+     protected $with=['mascotas'];
+
+    public $incrementing=true;
+    public $timestamps=false;
 
     public $fillable=[
     	'nombre',
@@ -22,5 +26,9 @@ class Propietario extends Model
     	'nummit',
     	'localidad'
     ];
+
+   public function mascotas(){
+        return $this->hasMAny(Mascota::class, 'id_propietario' , 'id_propietario');
+    }
 
 }
